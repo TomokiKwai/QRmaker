366fa0dfb51e9d79f7158571429ddfcfbaae37fe
TomokiKwai <tomojp1428@gmail.com>
2020-10-27 12:13:03 +0900

    first commit

---
+++ WindowsFormsApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public static class Program
+    {
+        public static void Launch()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 form = new Form1();
+            form.Show();
+        }
+    }
+}
